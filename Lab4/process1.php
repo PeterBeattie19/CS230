a0fb7aea577a3a72320119050f10c83f04e1991f
Peter Beattie <peterbeattie19@gmail.com>
2018-04-16 20:59:14 +0100

    Add files via upload

---
+++ Lab4/process1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+
+<?php
+	
+	session_start();
+
+
+	$total = 0;
+
+	for($i = 0; $i < 20; $i++){
+		if($_POST["".$i] == "correct")
+			$total++; 
+	}
+
+	$userName = $_SESSION['name']; 
+	$string = file_get_contents("JasonFile.json");
+	$object = json_decode($string, true);
+
+	$elementCount  = count($object);
+	$userExists = false; 
+	$index = 0;
+
+
+
+
+
+	for ($x = 0; $x < $elementCount; $x++) {
+    	if($object[$x]["name"] == $userName){
+    		$index = $x;
+    		break; 
+    	}
+	}
+
+	$_SESSION['id'] = $index; 
+
+	$object[$index]["score"] += $total;
+
+	array_push($object[$index]["history1"],$total); 
+
+	file_put_contents('JasonFile.json', json_encode($object));
+
+
+	echo "<p> Yiu Scored ".$total."</p>";
+?>
+<body>
+
+
+
+	<form action="UserHistory.php" method="get" align="center"> 
+		<input type="submit" value="View Yiur History"> 
+	</form>
+
+
+
+
+</body>
+</html>
